4dd6e4c1958a6c7b8c2762b9d6235537585f79d8
aoshea <aoshea@users.noreply.github.com>
2023-02-20 09:23:30 +0000

    1 file modified

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // run `node index.js` in the terminal
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+console.log(`!Hello Node.js v${process.versions.node}!`);
 
 const requester = require('./request_dict');
 requester

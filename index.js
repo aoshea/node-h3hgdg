7b827980fbd5f972b015d38ee82900697fbf411e
Alastair O'Shea <alastair.oshea@gmail.com>
2023-06-13 10:16:16 +0100

    fix ordered word key bug

--- index.js
+++ index.js
@@ -11,66 +11,76 @@ const { findConnectors } = require('./connectors');
 const { Trie } = require('./trie');
 
 function initWordsets(wordsets) {
-  const ordered = wordsets.slice(0, 1);
-  for (let i = 1; i < wordsets.length; ++i) {
-    const x = wordsets[i];
-    let prev = ordered[i - 1];
-    const chars = x.split('');
-    for (let j = 0; j < chars.length; ++j) {
-      const ch = chars[j];
-      if (prev.indexOf(ch) === -1) {
-        prev = prev + ch;
-      }
-    }
-    ordered.push(prev);
-  }
-  return ordered;
+	const ordered = wordsets.slice(0, 1);
+	for (let i = 1; i < wordsets.length; ++i) {
+		const chars = wordsets[i].split('');
+		const prevChars = ordered[i - 1].split('');
+
+		let node;
+		let charIndex;
+		let result = '';
+		while (chars.length > 0) {
+			let charToAdd;
+			if (prevChars.length > 0) {
+				node = prevChars.shift();
+				charIndex = chars.indexOf(node);
+				if (charIndex === -1) {
+					throw new Error('All prev chars must exist in current chars');
+				}
+				charToAdd = chars.splice(charIndex, 1)[0];
+			} else {
+				charToAdd = chars.shift();
+			}
+			result = result + charToAdd;
+		}
+
+		ordered.push(result);
+	}
+	return ordered;
 }
 
 const INPUT =
-  'prime impure premium prim mire rip umpire pi absolutely empire cheese dim mid dime idem deism dimes missed misused surmised';
+	'prime impure premium prim mire rip umpire pi absolutely empire cheese dim mid dime idem deism dimes missed misused surmised';
 requester
-  .requestDict()
-  .then((res) => {
-    const trie = new Trie();
-    const indexed = createIndexedDict(INPUT, ' ');
-    for (const key in indexed) {
-      trie.insert(key);
-    }
-    const connectors = findConnectors(indexed);
+	.requestDict()
+	.then(res => {
+		const trie = new Trie();
+		const indexed = createIndexedDict(res);
+		for (const key in indexed) {
+			trie.insert(key);
+		}
+		const connectors = findConnectors(indexed);
 
-    let data = '';
-    let count = 0;
+		let data = '';
+		let count = 0;
 
-    for (let i = 0; i < connectors.length; ++i) {
-      ++count;
-      let answers = [];
-      let wordsets = [];
-      const connx = connectors[i];
-      for (const ok of connx) {
-        answers.push(indexed[ok]);
-        wordsets.push(ok);
-      }
-      answers = Array.prototype.concat.apply([], answers);
-      answers = answers
-        .filter((item, index, list) => list.indexOf(item) === index)
-        .join('|');
-      wordsets = initWordsets(
-        wordsets.filter((item, index, list) => list.indexOf(item) === index)
-      ).join('|');
+		for (let i = 0; i < connectors.length; ++i) {
+			++count;
+			let answers = [];
+			let wordsets = [];
+			const connx = connectors[i];
+			for (const ok of connx) {
+				answers.push(indexed[ok]);
+				wordsets.push(ok);
+			}
+			answers = Array.prototype.concat.apply([], answers);
+			answers = answers.filter((item, index, list) => list.indexOf(item) === index).join('|');
+			wordsets = initWordsets(
+				wordsets.filter((item, index, list) => list.indexOf(item) === index)
+			).join('|');
 
-      data += wordsets + ',' + answers + '\n';
-    }
+			data += wordsets + ',' + answers + '\n';
+		}
 
-    fsPromises
-      .writeFile('games.csv', data)
-      .then((res) => {
-        console.log('done writing', res);
-      })
-      .catch((err) => {
-        console.error('error writing', err);
-      });
-  })
-  .catch((err) => {
-    console.log('catch err', err);
-  });
+		fsPromises
+			.writeFile('games.csv', data)
+			.then(res => {
+				console.log('done writing', res);
+			})
+			.catch(err => {
+				console.error('error writing', err);
+			});
+	})
+	.catch(err => {
+		console.log('catch err', err);
+	});
